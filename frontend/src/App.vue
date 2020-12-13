b59db9943129ce5d67f848d08ac2fdb57ce8f21f
Kyle Coberly <kyle.coberly@gmail.com>
2020-12-13 16:43:10 -0700

    Refactoring to use HTML and CSS

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,87 +1,70 @@
 <template>
   <div id="the-app">
     <h1>Sika Shoes</h1>
-    <NavigationBar
-      :currentPage="currentPage"
-      @navigate="navigate"
-    />
+    <nav class="navigation-bar">
+      <ul>
+        <li><a href="#" class="active">Home</a></li>
+        <li><a href="#" class="">Products</a></li>
+        <li><a href="#" class="">About</a></li>
+      </ul>
+    </nav>
     <main>
-      <component :is="currentPage" :data="currentPageProps" />
+      <div id="home-page">
+        <header class="hero-header">
+          <img src="https://media.bizj.us/view/img/11379549/rothys*800xx662-374-0-56.jpg" alt="Hero">
+          <h2>Shoes just got cool again.</h2>
+        </header>
+        <p>
+          Shoes. Everybody wants them. The problem is, no one can figure out where to buy them!
+          That's why we created Sika Shoes. All you need to do is get the key from Gary, go down
+          to the basement, unlock the door, plug in the computer, and order some shoes. Let's
+          get shoes on our feet!
+        </p>
+      </div>
+      <div id="product-page">
+        <h2>Products</h2>
+        <ul>
+          <li>
+            <div class="product-listing">
+              <div class="image-container"><img src="https://images-na.ssl-images-amazon.com/images/I/51b%2BfaJAyJL._AC_UY395_.jpg" alt="Airwalks"></div>
+              <h3>Airwalks</h3>
+              <p>Time-travel to 1997 and let's go Skateboarding!</p>
+            </div>
+          </li>
+          <li>
+            <div class="product-listing">
+              <div class="image-container"><img src="https://m.media-amazon.com/images/I/71E5vDK4ucL._AC_UL1500_.jpg" alt="Xelero"></div>
+              <h3>Xelero</h3>
+              <p>
+                You can make fun of these shoes all you want, but I can't even
+                hear you from up here on these clouds I'm walking on.
+              </p>
+            </div>
+          </li>
+          <li>
+            <div class="product-listing">
+              <div class="image-container"><img src="https://cdn.shopify.com/s/files/1/0877/4986/products/002_ChiliRed_pdp_A.jpg?v=1596060959" alt="Rothy's"></div>
+              <h3>Rothy's</h3>
+              <p>Did you know you can put these in the washing machine?</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div id="about-page">
+        <p>
+          Sika Shoes is a small, fictional company that's commited to doing business
+          the old-fashioned way: By making it all up.
+        </p>
+      </div>
     </main>
   </div>
 </template>
 
 <script>
-import HomePage from '@/HomePage.vue';
-import ProductPage from '@/ProductPage.vue';
-import AboutPage from '@/AboutPage.vue';
-import NavigationBar from '@/NavigationBar.vue';
-
 export default {
   name: 'App',
-  components: {
-    HomePage,
-    ProductPage,
-    AboutPage,
-    NavigationBar,
-  },
-  data() {
-    return {
-      currentPage: 'home-page',
-      currentPageProps: {},
-    };
-  },
-  methods: {
-    navigate(destination) {
-      const pageProps = {
-        'home-page': {},
-        'product-page': {
-          products: this.products,
-        },
-        'about-page': {},
-      };
-      this.currentPageProps = pageProps[destination];
-      this.currentPage = destination;
-    },
-  },
-  computed: {
-    products() {
-      return [{
-        id: 1,
-        name: 'Airwalks',
-        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51b%2BfaJAyJL._AC_UY395_.jpg',
-        description: 'Time-travel to 1997 and let\'s go Skateboarding!',
-      }, {
-        id: 2,
-        name: 'Xelero',
-        imageUrl: 'https://m.media-amazon.com/images/I/71E5vDK4ucL._AC_UL1500_.jpg',
-        description: 'You can make fun of these shoes all you want, but I can\'t even hear you from up here on these clouds I\'m walking on.',
-      }, {
-        id: 3,
-        name: 'Rothy\'s',
-        imageUrl: 'https://cdn.shopify.com/s/files/1/0877/4986/products/002_ChiliRed_pdp_A.jpg?v=1596060959',
-        description: 'Did you know you can put these in the washing machine?',
-      }];
-    },
-  },
 };
 </script>
 
 <style lang="scss">
-@import "@/styles/_reset.scss";
-@import "@/styles/_typography.scss";
-@import "@/styles/_sizes.scss";
-
-#the-app {
-  display: flex;
-  flex-flow: column nowrap;
-  align-items: center;
-  padding: $xl;
-  h1 {
-    @include heading-font-1;
-  }
-  main {
-    max-width: 800px;
-  }
-}
 </style>
